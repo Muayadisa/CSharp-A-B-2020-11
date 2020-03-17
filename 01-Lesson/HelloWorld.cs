1fc328190fb26928244b73ee861be2067c7765cd
Raya Hazi <44358915+rayahazi@users.noreply.github.com>
2020-03-17 10:33:29 +0200

    Create HelloWorld.cs

---
+++ 01-Lesson/HelloWorld.cs
@@ -0,0 +1,18 @@
+// 'using' is to add a reference to external dlls. (dll = dynamic link library)
+using System;
+
+// namespace - מעטפת חיצונית של הקובץ
+namespace _01_HelloWorld1
+{
+    // class - המחלקה הראשית שמכילה את הפונקציה שתתחיל את ריצת התוכנית. 
+    class Program
+    {
+        // This is the main function. The first to run. 
+        static void Main(string[] args)
+        {
+            // Print output to the console: 
+            // Shortcut: c w tab tab: 
+            Console.WriteLine("Hello world");
+        }
+    }
+}
