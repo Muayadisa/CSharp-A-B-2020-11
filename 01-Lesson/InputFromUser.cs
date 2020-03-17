cf48ebed6d169e6e0341e968d84d55d1f4db3524
Raya Hazi <44358915+rayahazi@users.noreply.github.com>
2020-03-17 11:10:06 +0200

    Create InputFromUser.cs

---
+++ 01-Lesson/InputFromUser.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace _01_HelloWorld1
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            string str;
+
+            Console.WriteLine("Say something: ");
+
+            // `ReadLine() ` - a function that returns the input from the keyboard string. 
+            // note: ReadLine() - gets the input in a string shape. 
+            str = Console.ReadLine();
+
+            Console.WriteLine($"You said: {str}");
+
+            Console.WriteLine($"2+5 = {2+5}");
+        }
+    }
+}
